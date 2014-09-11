736a1c4f127ef50a303b08fcc26ecec304df7046
morganherlocker <morgan.herlocker@gmail.com>
2014-09-11 15:58:47 -0400

    generating proper geometries

--- index.js
+++ index.js
@@ -5,9 +5,26 @@ var fs = require('fs')
 var docs = JSON.parse(fs.readFileSync('./docs-in.json'));
 
 docs.forEach(function(doc) {
-	// get the geometry from _bbox and delete the tile indexes
+	/*// get the geometry from _bbox and delete the tile indexes
 	var geom = turf.bboxPolygon(doc._bbox).geometry
 	doc._geometry = geom;
+	delete doc._zxy*/
+
+	// get tilebelt style tiles
+	doc._zxy = doc._zxy.map(function(zxy) {
+		var zxyArr = zxy.split('/');
+		var xyz = [parseInt(zxyArr[1]), parseInt(zxyArr[2]), parseInt(zxyArr[0])];
+		return xyz;
+	});
+
+	// generate geometries for each of the tiles
+	var polygons = turf.featurecollection([]);
+	doc._zxy.forEach(function(tile){
+		polygons.features.push(tilebelt.tileToGeoJSON(tile))
+	})
+	var geom = turf.merge(polygons)
+	geom = turf.buffer(geom, -5, 'miles');
+	doc._geometry = geom.features[0].geometry;
 	delete doc._zxy
 });
 
